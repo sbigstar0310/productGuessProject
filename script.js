936635a8cbe22675dcecc6403db0a20dfb1008bd
sbigstar0310 <sbigstar0930@gmail.com>
2023-09-20 15:29:55 +0900

    initial implementation complete (serverless)

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-function test() {
-	var real_price = 29000;
-	var user_price = document.getElementById("user-price-box").value;
-	document.getElementById('real-price-box').value = real_price;
-	document.getElementById('result-box').value = user_price - real_price;
+let jsonData = JSON.parse(JSON.stringify(items));
+var random_index = Math.floor( ( Math.random() * 10 + 1 ) );
+//const random_index = 1;
+//console.log(random_index);
+var item_name = JSON.stringify(jsonData[random_index].name);
+var item_price = JSON.stringify(jsonData[random_index].price);
+var item_image = JSON.stringify(jsonData[random_index].image);
 
-}
+//console.log(item_name);
+//console.log(item_price);
+//console.log(item_image);
 
 function gameStart() {
-	// 게임영역 초기화
-	$("#gameArea").empty();
+	var imgArea = document.getElementById("productImageArea");
+	var nameArea = document.getElementById("productNameArea");
+	imgArea.innerHTML = "";
+	nameArea.innerHTML = "";
 
-	// 랜덤난수 부여
-	var randomNumber = Math.floor(Math.random() * 101);
-	$("#randomNumber").val(randomNumber);
+	var img = new Image();
+	img.src = 'images/' + item_image.replace(/"/g, '');
+	img.width  = "400";
+	img.height = "400";
+	imgArea.appendChild(img);
 
-	// 게임 영역 생성
-	$("#gameArea").append("<image src = 'images/1.jpg' width = '400' height = '400'></image>")
-	$("#gameArea").append("<p>삼성 정품 갤럭시 Z플립5 케이스 실리콘 With 링 제트플립5</p>");
+	nameArea.append(item_name);
 }
+
+function nextProblem() {
+	document.getElementById("user-price-box").value = "";
+	document.getElementById("real-price-box").value = "";
+	document.getElementById("diff-price-box").value = "";
+
+	random_index = Math.floor( ( Math.random() * 10 + 1 ) );
+	item_name = JSON.stringify(jsonData[random_index].name);
+	item_price = JSON.stringify(jsonData[random_index].price);
+	item_image = JSON.stringify(jsonData[random_index].image);
+
+	var imgArea = document.getElementById("productImageArea");
+	var nameArea = document.getElementById("productNameArea");
+	imgArea.innerHTML = "";
+	nameArea.innerHTML = "";
+
+	var img = new Image();
+	img.src = 'images/' + item_image.replace(/"/g, '');
+	img.width  = "400";
+	img.height = "400";
+	imgArea.appendChild(img);
+
+	nameArea.append(item_name);
+}
+
+function show_result() {
+	console.log("show_result function");
+	var real_price = item_price.replace(/"/g, "") * 1;
+	console.log("real_price: " + real_price + " type: " + typeof real_price);
+	var user_price = parseInt(document.getElementById("user-price-box").value);
+	console.log("user_price: " + user_price + " type: " + typeof user_price);
+	document.getElementById("real-price-box").value = real_price;
+	document.getElementById("diff-price-box").value = real_price - user_price;
+}
+
+
+
